413314680f792a214f4811f86ac67dcac2c6ff19
cplusplusstuff24 <cplusplusstuff24@gmail.com>
2024-08-15 14:50:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,45 +1,9 @@
 #include <iostream>
-#include <vector>
-
-// Todo class
-class Todo {
-  int m_id;
-  std::string m_name;
-  bool m_done;
-
-public:
-  Todo(int id, std::string name, bool done) {
-    m_id = id;
-    m_name = name;
-    m_done = done;
-  };
-
-  // Getter function for m_name
-  std::string get_name() const {
-    return m_name;
-  }
-};
-
-// Todos class
-class Todos {
-  std::vector<Todo> todos;
-
-  public:
-
-    void add_todo(Todo todo) {
-      todos.push_back(todo);
-    };
-
-    void print_todos() {
-      for (Todo todo : todos) {
-        std::cout << todo.get_name() << std::endl;
-      }
-    }
-};
+#include "todo.h"
+#include "todos.h"
 
 int main() {
-
-  std::cout << "Hello World!\n";
+  
   Todo todo1 = Todo(1, "Learn C++", false);
   Todo todo2 = Todo(2, "Learn Python", false);
   Todo todo3 = Todo(3, "Learn Java", false);
@@ -51,4 +15,3 @@ int main() {
 
   todos.print_todos();
 }
-
